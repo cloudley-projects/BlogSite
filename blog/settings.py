e34920c791d338a9c944b80f5d5beeb18272216e
Cloudley <cloudict01@gmail.com>
2022-09-28 18:46:14 +0000

    Added env variable option for db and secret key

--- blog/settings.py
+++ blog/settings.py
@@ -12,19 +12,23 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import logging
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# Create the loggers object
+logger = logging.getLogger('__name__')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l31o(vihv8z5r*rkgp(8v3&bkou%60t%#3f3($wm0_y@zuc9@$'
+
+SECRET_KEY = os.environ.get('SECRET_KEY','l31o(vihv8z5r*rkgp(8v3&bkou%60t%#3f3($wm0_y@zuc9@$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = []
 
@@ -75,15 +79,27 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-		'read_default_file': '/etc/mysql/my.cnf'
-},
+if os.environ.get('DB_TYPE') == 'cloudsql':
+    DATABASES = {
+        'default': {
+            'ENGINE'  : 'mysql.connector.django', # <-- UPDATED line 
+            'NAME'    : os.environ['DB_NAME'],              # <-- UPDATED line 
+            'USER'    : os.environ['DB_USER'],                     # <-- UPDATED line
+            'PASSWORD': os.environ['DB_PASSWORD'],              # <-- UPDATED line
+            'HOST'    : os.environ['DB_HOST'],                # <-- UPDATED line
+            'PORT'    : os.environ['DB_PORT']
+        }
     }
-}
-
+    logger.warning('Environment variable is set and CloudSQL database is being used')
+    
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+    logger.warning('Environment variable is not set and default database is being used')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
